af43d958ed9efe6b2526e20d229556b7490a1468
Francis Lagacé <francis.lagace94@gmail.com>
2022-01-19 16:11:44 -0500

    webapi – commit initial

---
+++ src/Web2.API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web2.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Web2.API/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web2.API.Models
+{
+    public class Product
+    {
+    }
+}
